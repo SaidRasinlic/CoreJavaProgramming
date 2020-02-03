9dfa3e7be47628c76e0207a2348a249d507e23f5
SaidRasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2020-02-04 00:43:37 +0100

    Ovdje se nalaze svi zadaci sa prezentacija Java Core Programming , plus dodatni primjeri koje sam ja radio, nisam ispostovao pravilo imenovanja paketa i klasa ...

---
+++ src/svastara/zadaci/Svastara.java
@@ -0,0 +1,360 @@
+package svastara.zadaci;
+
+/*package rectangleareaexample;
+import java.util.Scanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
+public class RectangleAreaExample {*/
+
+   
+  //  public static void main(String[] args) {
+       /*   Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner.nextInt () * scanner.nextInt());
+    int variableA = 1212;
+    int variableB = 1212;
+    int result = variableA+variableB;
+    System.out.println(result);
+    boolean pablo;
+    pablo = true;
+    pablo=!pablo;
+    System.out.println(said);
+    char[]brat = {'s', ' ','a','b','0'};
+    System.out.println(brat); */
+        //int a=2,b=3;
+        //int res =a*(b++)+b;
+        //int res1=a*(++b)+b;
+        // System.out.println(res);
+        // System.out.println(res1);
+        /* String korisničkiUnos = JOptionPane.showInputDialog("Unesite neki broj, molim vas.");
+    int uneseniBroj = Integer.parseInt(korisničkiUnos);
+    String imeDanaUSedmici = "unknown";
+    switch (uneseniBroj) {
+    case 1:
+    imeDanaUSedmici = "Ponedjeljak";
+    break;
+    case 2:
+    imeDanaUSedmici = "Utorak";
+    break;
+    case 3:
+    imeDanaUSedmici = "Srijeda";
+    break;
+    case 4:
+    imeDanaUSedmici = "Četvrtak";
+    break;
+    case 5:
+    imeDanaUSedmici = "Petak";
+    break;
+    case 6:
+    imeDanaUSedmici = "Subota";
+    break;
+    case 7:
+    imeDanaUSedmici = "Nedjelja";
+    break;
+    default:
+    imeDanaUSedmici = "MAMA NE LUPAJJJJ";
+    break;
+    }
+    System.out.println("Ime dana koji odgovara broju "+ uneseniBroj + " je : " + imeDanaUSedmici);
+    // znači ovo je SWITCH CASE naredba i dobro je nauči sinčino */
+ /*String namesAndSurnameArray [][] = {
+    {"Messi" ,"Ronaldo" ,"Pogba"},
+    {"Leo","Cristiano","Paul"}
+    };
+    System.out.println(namesAndSurnameArray[0][0]+" " + namesAndSurnameArray[1][0]);
+  
+     char[] copyFrom = {'d', 'e', 'c', 'a', 'f', 'f', 'e', 't', 'i', 'n', 'a', 'f', 'r'};
+   char[] copyToArray = new char[7];
+    System.out.println(new String(copyToArray));
+    System.arraycopy(copyFrom, 2, copyToArray,0,7); 
+    ---------------------------------------------------------------------------
+    char[] copyToArray = Arrays.copyOfRange(copyFrom, 2, 10);
+    System.out.println(new String(copyToArray)); */
+
+ /*  Scanner newScanner = new Scanner(System.in);
+   
+    int uneseniBroj;
+    do{
+       System = newScanner.nextInt();
+    }while (uneseniBroj!=5);
+    System.out.println("Poceo igricu"); .out.println("Pritisnu peticu zapocni igricu");
+    uneseniBroj = newScanner.nextInt();
+    }while (uneseniBroj!=5);
+    System.out.println("Poceo igricu"); 
+     
+/*char [] kopirajOvdje = Arrays.copyOfRange(copyFrom, 2, 10);
+    System.out.println(new String (kopirajOvdje));
+    int [] numbers = {12,41,35,13523,125,124};
+    for(int mutant : numbers) {
+    System.out.println(mutant);
+    System.out.print("9" + " ");
+    System.out.print("8" + " ");
+    System.out.print("7" + " ");
+    System.out.print("6" + " ");
+    System.out.print("5" + " ");
+    System.out.print("4" + " ");
+    System.out.print("3" + " ");
+    System.out.print("2" + " ");
+    System.out.print("1" + " ");
+    System.out.print("0" + " "); 
+    String mojPass = "dv ulfmw z givzhgiv";
+    System.out.println(mojPass);
+    
+ String str = null;
+System.out.println(str); 
+
+
+int numbers[] = { 1, 2, 3, 4, 5 };
+ 
+Arrays.fill(numbers, 1,3,5);
+System.out.println(Arrays.toString(numbers)); 
+byte[] famousNumbers = { 0, 1, 2, 4, 8, 16, 32, 64 };
+String arrayAsString = Arrays.toString(famousNumbers); // �[0, 1, 2, 4, 8, 16, 32, 64]
+System.out.println(arrayAsString); */
+ /*int said = 1;
+        while(said<4) {
+        System.out.println("ok ok");
+      said++;
+} 
+        System.out.println("vani si");  */
+ /*   int count = 21;
+       do {
+           System.out.println("Count is: " + count);
+          count++;
+           }while(count<11); 
+        
+       // for (int i=1;i<10;i++) {
+            // System.out.println("Bravo majstore brojevi su " + i); 
+        int[] numbers = {1123, 23, 34, 556, 78, 98, 90, ',', 234, 23, 454, 6, 6};
+        int searchFor = new Scanner(System.in).nextInt();
+      boolean pronadjen = false;
+        int i;
+        
+       
+        for (i = 0; i < numbers.length; i++) {
+             
+            
+            int mutant = numbers[i];
+            if (mutant == searchFor) {
+               
+                pronadjen = true;
+                break;
+            }
+        }
+        if (pronadjen) {
+
+            System.out.println("Pronadjen " + searchFor + " na indeksu '" + i + "'");
+        } else {
+            System.out.println("Nema tog broja kojeg ti potrazujes " + searchFor); 
+        System.out.println("Unesi neki broj");
+        int searchFor = new Scanner(System.in).nextInt();
+        int[][] numbers = {
+            {32, 87, 3, 589},
+            {12, 34, 655, 87, 8},
+            {23, 4, 5, 7, 899, 3453}
+        };
+        search_for:
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                int vrijednost = numbers [i][j];
+                System.out.print(vrijednost + "    "); 
+                if (searchFor==vrijednost){
+                  
+                    break search_for;n*/
+ /* FOR PETLJA
+    for (int i=10; i<=100; i=i+10)
+ {
+ System.out.println(i);
+    for (int i=1; i<=10; i++)
+ {
+ System.out.println(i);
+ }
+    WHILE PETLJA
+ 
+int i = 10;
+while(i<=100){
+    System.out.println(i + " = +10");
+    i=i+10; 
+ 
+int i = 10;
+ do {
+ System.out.println(i);
+ i=i+10;
+ } while(i <= 100); 
+ 
+ double x = 113.0/112.0;
+    System.out.println("Hello from number " + x);
+    boolean isBetter = !true;
+            System.out.println(isBetter); 
+            int username = 123;
+            int dbusername = 321;
+            if(username==dbusername) {
+                System.out.println("Successful login"); 
+ 
+ boolean hero_is_dead = true;
+ if (hero_is_dead) {
+     System.out.println("Hero is dead. REVIVING.");
+     hero_is_dead = false;
+            }else System.out.println("ITS NOT DEAD "); 
+ 
+ int i;
+ for(i=0;i<=5;i++){ 
+    System.out.println("i is " + i); 
+         System.out.println("********************************");
+        System.out.printf("%-10c%-10d%-10d%-10d\n", '#', 1, 2, 3);
+        System.out.println("********************************");
+        for (int i = 1; i <= 10; ++i) {
+            System.out.printf("%-10d", i);
+            for (int j = 1; j <= 3; ++j) {
+                System.out.printf("%-10d", i * j);
+            }
+
+            System.out.print("\n");
+            System.out.println(" ");
+            
+        int[][] numbers = {
+            {32, 87, 3, 589},
+            {12, 34, 655, 87, 8},
+            {23, 4, 5, 7, 899, 3453}
+        };
+        System.out.println("Unesi neki broj");
+        int korisnickiUnos = new Scanner(System.in).nextInt();
+        boolean foundIt = false;
+        int i = 0;
+        int j = 0;
+
+        labeliramTe:
+        
+        
+        for (; i < numbers.length; i++) {
+
+            int[] vrijednost = numbers[i];
+            for ( j = 0; j < vrijednost.length; j++) {
+                int vrijednostNiza = vrijednost[j];
+                if (vrijednostNiza == korisnickiUnos) {
+                    foundIt = true;
+                    break labeliramTe;
+
+                }
+
+            }
+
+        }
+        String poruka;
+        if (foundIt) {
+            poruka = "postoji broj '" + korisnickiUnos + "' na poziciji '" + i + "," + j + "'";
+        } else {
+            poruka = "ne postoji '" + korisnickiUnos + "'";
+        }
+        System.out.println(poruka); 
+        String recenica = JOptionPane.showInputDialog("Unesi recenicu.");
+        String slovo = JOptionPane.showInputDialog("Unesi slovo.");
+        if (slovo.length() != 1 && slovo.length() != 0) {
+            JOptionPane.showMessageDialog(null, "Unesi slovo, a ne recenicu.");
+
+        } else {
+            int count = 0;
+            for (int i = 0; i < recenica.length(); i++) {
+                char slovoURecenici = recenica.charAt(i);
+                if (slovoURecenici != slovo.charAt(0)) {
+                    continue;
+
+                }
+                count++;
+            }
+                 JOptionPane.showMessageDialog(null, "Slovo u recenici se pojavljuje '" + count +"' puta"); 
+        String recenica = JOptionPane.showInputDialog("Unesi neku recenicu");
+        String rijec = JOptionPane.showInputDialog("Unesi rijec");
+        boolean input = false;
+        labeliranaPetlja:
+        for (int i = 0; i < recenica.length(); i++) {
+            int k = i;
+
+            for (int j = 0; j < rijec.length(); j++) {
+                if (recenica.charAt(k++) != rijec.charAt(j)) {
+
+                    continue labeliranaPetlja;
+                }
+
+            }
+            input = true;
+            break;
+        } 
+        System.out.println(input ? "Sadrzana" : "Nije sadrzana"); 
+        byte[] said = { 0, 1, 2, 4, 8, 16, 32, 64 };
+
+System.out.println(Arrays.toString(said)); 
+ long[] bigNumbers = { 200000000L, 400000000L, 100000000L, 300000000L }; // it's unsorted
+ 
+Arrays.sort(bigNumbers); // sorting whole array
+ 
+System.out.println(Arrays.toString(bigNumbers));
+int[] numbers1 = { 1, 2, 5, 8 };
+int[] numbers2 = { 1, 2, 5 };
+int[] numbers3 = { 1, 2, 5, 8 };
+ 
+System.out.println(Arrays.equals(numbers1, numbers2)); // it prints "false"
+System.out.println(Arrays.equals(numbers1, numbers3)); // it prints "true" 
+
+  boolean arr[] = new boolean[] {true, true, true, true, true};
+  
+      // let us print the values
+      System.out.println("Actual values: ");
+      for (boolean value : arr) {
+         System.out.println("Value = " + value);
+      }
+
+      // using fill for placing false from index 1 to 4
+      Arrays.fill(arr, 1, 3, false);
+
+      // let us print the values
+      System.out.println("New values after using fill() method: ");
+      for (boolean value : arr) {
+         System.out.println("Value = " + value);   
+ //   --------------------------------------------------------------------------
+        int size = 12;
+        char[] characters = new char[size];
+
+// It takes an array, start index, end index (exclusive) and the value for filling the array
+        Arrays.fill(characters, 0, size / 2, 'A');
+        Arrays.fill(characters, size / 2, size, 'B');
+
+        System.out.println(Arrays.toString(characters)); // it prints [A, A, A, A, A, B, B, B, B, B] 
+        
+        /*
+    another comment
+ said  said  rasinlic  rasinlic 
+
+        int a = 5, b = 3, c = 5;
+        int res = a * b + c - a;
+        System.out.println(res);
+        res = a * (b + c - a);
+        System.out.println(res);
+               
+   System.out.println("********************************");
+        System.out.printf("%-10c%-10d%-10d%-10d\n", '#', 1, 2, 3);
+        System.out.println("********************************");
+        for (int i = 1; i <= 10; ++i) {
+            System.out.printf("%-10d", i);
+            for (int j = 1; j <= 3; ++j) {
+                System.out.printf("%-10d", i * j);
+            }
+
+            System.out.print("\n"); */
+ 
+
+
+    /*public static void main(String[] args) {
+        String[] array = {"Heart", "Cherry", "Coin", "Melon", "Jocker"};
+        System.out.println("Combination is: ");
+        for (int i = 0; i < array.length; ++i) {
+            Object randomNumber = new Object();
+            int choosen = randomNumber.nextInt(array.length);
+            System.out.println(array[choosen] + " "); 
+            
+        }
+    }
+}
+*/
