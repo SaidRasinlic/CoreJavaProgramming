554819d86bb8627c4e21fdcea6f95974f2615c45
SaidRasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2020-02-04 00:44:02 +0100

    Ovdje se nalaze svi zadaci sa prezentacija Java Core Programming , plus dodatni primjeri koje sam ja radio, nisam ispostovao pravilo imenovanja paketa i klasa ...

---
+++ src/vraperi/WrapTime.java
@@ -0,0 +1,44 @@
+package vraperi;
+
+
+public class WrapTime {
+
+    public static void main(String[] args) {
+
+        /*       Integer my_integer = 10;
+        my_integer +=25;
+     //   System.out.println(my_integer);
+        
+        
+        Integer my_int = new Integer("121");
+        //System.out.println(my_int);
+        
+        
+        
+        float x  = my_integer.floatValue();
+        double y = my_integer.doubleValue();
+        byte z = my_integer.byteValue();
+        
+        int i = 35;
+        
+  //      System.out.println(my_integer.compareTo(i));
+    //     System.out.println(my_integer==i);
+        
+         
+         int parsed_int = Integer.parseInt("100");
+         System.out.println(parsed_int);
+         System.out.println(Integer.SIZE);
+         System.out.println(Integer.MAX_VALUE);
+         System.out.println(Integer.BYTES); */
+        System.out.println(Math.ceil(1.05)); // 2 zaokruzuje najveci
+        System.out.println(Math.floor(2.999));  // 2 zaokuruzuje smanji
+        System.out.println(Math.pow(3, 5));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+
+    }
+
+}
+
+
